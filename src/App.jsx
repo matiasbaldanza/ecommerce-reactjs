40f9dcbd328b8c5f443471487ec090e05faa00c6
Matias Baldanza <matiasbaldanza@gmail.com>
2023-03-23 08:37:47 -0300

    feat agregar ItemCount

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,17 @@ import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import Greeting from './components/Greeting/Greeting'
 
+import ItemCount from './components/ItemCount/ItemCount'
+
 function App () {
   return (
     <div className='container m-auto max-w-7xl'>
       <NavBar />
       <ItemListContainer>
         <Greeting />
+        <ItemCount initial={1} stock={10} onAdd={(count) => console.log('Cantidad agregada ', count)} />
       </ItemListContainer>
+
     </div>
   )
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ItemListContainer ({ ...props }) {
   return (
     <div
-      className='flex flex-col items-center w-full h-screen'
+      className='flex flex-col items-center w-full gap-4'
     >
       {props.children}
     </div>
