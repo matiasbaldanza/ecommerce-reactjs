5bf9aa32f221ec84d678da0b67eafd44e905fc20
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-23 15:59:26 -0300

    refactor: extraer ProductSpecs para ItemDetail

---
+++ src/components/ProductSpecs/ProductSpecs.jsx
@@ -0,0 +1,46 @@
+import Balancer from 'react-wrap-balancer'
+
+function ProductSpecs ({ features }) {
+  return (
+    <section className='flex flex-col items-center gap-4'>
+      <h2 className='text-2xl card-title'>
+        Especificaciones
+      </h2>
+      <div className='grid grid-cols-4 gap-6'>
+        {
+      Object.entries(features).map(([key, value]) => {
+        return (
+          <div
+            key={key}
+            className='relative flex flex-row gap-2 p-6 pt-16 card bg-slate-100'
+          >
+            <div
+              className='absolute w-16 rounded-lg top-1 right-1 aspect-square'
+            >
+              <img
+                src={`/public/feature-icons/${key}.png`} alt='cpu-icon'
+              />
+            </div>
+            <div>
+              <h3 className='pb-2 text-xl card-title text-primary'>
+                <Balancer>{value.title}</Balancer>
+              </h3>
+              <p
+                className='text-sm'
+              >
+                <Balancer>
+                  {value.text.split('\n').map((line, index) => <span key={index}>{line}<br /></span>)}
+                </Balancer>
+              </p>
+            </div>
+          </div>
+        )
+      }
+      )
+}
+      </div>
+    </section>
+  )
+}
+
+export default ProductSpecs
