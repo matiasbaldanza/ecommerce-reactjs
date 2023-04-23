1a286022b8eb0b731a04137b09214032b362cd3f
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-23 20:22:50 -0300

    feat: agregar AnnouncementBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Components
+import AnnouncementBar from './components/AnnouncementBar/AnnouncementBar'
 import NavBar from './components/NavBar/NavBar'
 import Greeting from './components/Greeting/Greeting'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
@@ -11,8 +12,13 @@ import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailCont
 
 function App () {
   return (
-    <div className='container m-auto max-w-7xl'>
-      {/*       <BrowserRouter>
+    <div>
+      <AnnouncementBar className='relative'>
+        <p>⚠️ En construcción! ⚠️</p>
+
+      </AnnouncementBar>
+      <div className='container m-auto max-w-7xl'>
+        {/*       <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path='/' element={<ItemListContainer />} />
@@ -22,19 +28,20 @@ function App () {
         </Routes>
       </BrowserRouter> */}
 
-      {/* TEST ItemListCointainer */}
-      {/*       <ItemListContainer>
+        {/* TEST ItemListCointainer */}
+        {/*       <ItemListContainer>
         <Greeting />
       </ItemListContainer> */}
 
-      {/* TEST ItemDetailContainer */}
-      {/* <ItemDetailContainer productId='alienware-m15-r7-240hz-core-i7-12700h-16gb-512gb-rtx-3070ti' /> */}
-      {/* <ItemDetailContainer productId='msi-pulse-gl76-17-3-144hz-i7-12th-gen-16gb-512gb-rtx-3070-8gb' /> */}
-      {/* <ItemDetailContainer productId='lenovo-ideapad-gaming-3-120hz-ryzen-5-8gb-256gb-rtx-3050ti' /> */}
-      {/* <ItemDetailContainer productId='asus-vivobook-pro-14-oled-core-i5-11300h-8gb-256gb' /> */}
-      {/* <ItemDetailContainer productId='asus-zenbook-flip-2en1-ryzen-7-5700u-8gb-256gb-geforce-mx450' /> */}
-      <ItemDetailContainer productId='lenovo-ideapad-3-14-ryzen-5-8gb-256-gb-ssd' />
+        {/* TEST ItemDetailContainer */}
+        {/* <ItemDetailContainer productId='alienware-m15-r7-240hz-core-i7-12700h-16gb-512gb-rtx-3070ti' /> */}
+        {/* <ItemDetailContainer productId='msi-pulse-gl76-17-3-144hz-i7-12th-gen-16gb-512gb-rtx-3070-8gb' /> */}
+        {/* <ItemDetailContainer productId='lenovo-ideapad-gaming-3-120hz-ryzen-5-8gb-256gb-rtx-3050ti' /> */}
+        {/* <ItemDetailContainer productId='asus-vivobook-pro-14-oled-core-i5-11300h-8gb-256gb' /> */}
+        {/* <ItemDetailContainer productId='asus-zenbook-flip-2en1-ryzen-7-5700u-8gb-256gb-geforce-mx450' /> */}
+        <ItemDetailContainer productId='lenovo-ideapad-3-14-ryzen-5-8gb-256-gb-ssd' />
 
+      </div>
     </div>
   )
 }
