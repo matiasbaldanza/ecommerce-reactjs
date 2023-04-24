df581174b5933add86fe00c24d9f10fce7eaefce
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-24 18:00:40 -0300

    style: usar TextWithLineBreaks en títulos en Item

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 
 // Componentes
+import TextWithLineBreaks from '../TextWithLineBreaks/TextWithLineBreaks'
 import Price from '../Price/Price'
 import ProductImage from '../ProductImage/ProductImage'
 
@@ -10,7 +11,7 @@ function Item ({ ...props }) {
     <article className='shadow-xl w-72 card bg-base-100'>
       <ProductImage id={id} src={images[0]} alt={name} />
       <div className='card-body'>
-        <h2 className='card-title'>{name}</h2>
+        <h2 className='card-title'><TextWithLineBreaks>{name}</TextWithLineBreaks></h2>
         <Price price={price} currency={priceCurrency} />
         <p>Stock disponible: {stock}</p>
         <footer className='flex justify-center mt-6 card-actions'>
