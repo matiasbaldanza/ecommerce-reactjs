b5fdbd71e1867d0a2adbb6b12d861064e9042ae0
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-16 17:46:07 -0300

    feat: CategoriesMenu es un menyu en desktop, y un menu desplegable position:fixed bottom en mobile

--- src/modules/store/CategoriesMenu/CategoriesMenu.jsx
+++ src/modules/store/CategoriesMenu/CategoriesMenu.jsx
@@ -2,21 +2,39 @@ import { NavLink } from 'react-router-dom'
 
 function CategoriesMenu ({ menuItems }) {
   return (
-    <ul className={`menu menu-horizontal menu-compact bg-background rounded-box mt-4
-                    sm:menu-horizontal sm:menu-normal sm:mt-0`}
-    >
-      {
-        menuItems.map((menuItem) => {
-          return (
-            <li
-              key={`categories-menu-${menuItem.route}`}
-            ><NavLink to={`/category/${menuItem.route}`}>{menuItem.title}</NavLink>
-            </li>
-          )
-        })
-      }
-    </ul>
+    <>
+      {/* Top menu para modo desktop */}
+      <ul className={`hidden rounded-box 
+                      mt-4 sm:menu sm:menu-horizontal sm:menu-normal sm:mt-0`}
+      >
+        <MenuItems items={menuItems} />
+      </ul>
+
+      {/* Menú desplegable fijo para mobile, position:fixed */}
+      <div className='fixed z-50 dropdown dropdown-top bottom-4 left-4'>
+        <label
+          tabIndex={0}
+          className={`m-1 btn btn-md btn-primary
+                      sm:hidden`}
+        >Categorías
+        </label>
+        <ul tabIndex={0} className='w-48 p-2 shadow dropdown-content menu bg-base-100 rounded-box'>
+          <MenuItems items={menuItems} />
+        </ul>
+      </div>
+    </>
   )
 }
 
+function MenuItems ({ items }) {
+  return items.map((item) => {
+    return (
+      <li key={`categories-menu-${item.route}`}>
+        <NavLink to={`/category/${item.route}`}>{item.title}</NavLink>
+      </li>
+    )
+  })
+}
+
+/* menu menu-vertical rounded-box mt-4 menu-compact */
 export default CategoriesMenu
