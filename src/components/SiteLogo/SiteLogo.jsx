90f62b6e7468f0d6298554e3bd1ea48039fc874b
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-16 11:40:49 -0300

    style: global colors

--- src/components/SiteLogo/SiteLogo.jsx
+++ src/components/SiteLogo/SiteLogo.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function SiteLogo () {
   const styles = `flex flex-col items-center justify-center gap-0 
-                  sm:ml-4 p-4 text-white border-8 border-white rounded-full aspect-square 
+                  sm:ml-4 p-4 text-white border-8 border-background rounded-full aspect-square 
                   bg-gradient-to-t from-blue-700 to-blue-300`
 
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['class'],
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))'
+      }
+    }
   },
   plugins: [require('daisyui')]
 }
