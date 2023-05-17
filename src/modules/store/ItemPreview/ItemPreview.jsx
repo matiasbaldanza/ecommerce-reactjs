20c3377f99130378f84dba21f4d4c2a1b6ceac98
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-16 21:55:13 -0300

    refactor: modificar ItemPreview para que acepte className como prop (ya la pasaba a ProductImage)

--- src/modules/store/ItemPreview/ItemPreview.jsx
+++ src/modules/store/ItemPreview/ItemPreview.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import clsx from 'clsx'
 
 // Componentes
 import ProductImage from '@/modules/item/ProductImage/ProductImage'
 
-function ItemPreview ({ id, name, images }) {
+function ItemPreview ({ id, name, images, ...props }) {
   const [isHovered, setIsHovered] = useState(false)
+  const styleHover = `absolute inset-0 transition-opacity duration-300 ease-in-out ${isHovered ? 'opacity-100' : 'opacity-0'}`
 
   const handleMouseEnter = () => setIsHovered(true)
   const handleMouseLeave = () => setIsHovered(false)
@@ -19,12 +21,13 @@ function ItemPreview ({ id, name, images }) {
         id={id}
         src={images[0]}
         alt={name}
+        className={props.className}
       />
       <ProductImage
         id={id}
         src={images[1]}
         alt={name}
-        className={`absolute inset-0 transition-opacity duration-300 ease-in-out ${isHovered ? 'opacity-100' : 'opacity-0'}`}
+        className={clsx(styleHover, props.className)}
       />
     </div>
   )
