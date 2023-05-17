88a64083c6ee5da987a68a295d9a5435cd99ed80
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-17 09:28:55 -0300

    refactor: modularizar CartWidget y hacer que el badge de cantidad sólo se muestre si hay items en el carrito

--- src/modules/cart/CartWidget/CartWidget.jsx
+++ src/modules/cart/CartWidget/CartWidget.jsx
@@ -5,7 +5,7 @@ import { pluralize } from '../../../utils/textUtils'
 import clsx from 'clsx'
 
 function CartWidget ({ ...props }) {
-  const { cart, cartQuantity } = useContext(CartContext)
+  const { cartQuantity } = useContext(CartContext)
   const styles = clsx('dropdown sm:dropdown-end dropdown-start',
     props.className)
 
@@ -17,8 +17,9 @@ function CartWidget ({ ...props }) {
       {/* Badge */}
       <label tabIndex={0} className='btn btn-ghost btn-circle'>
         <div className='indicator'>
-          <svg xmlns='http://www.w3.org/2000/svg' className='w-5 h-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z' /></svg>
-          <span className='font-extrabold border-0 text-brand sm:text-background bg-background sm:bg-foreground badge badge-sm indicator-item'>{cartQuantity()}</span>
+          <CartIcon />
+          {cartQuantity() > 0 &&
+            <CartBadge>{cartQuantity()}</CartBadge>}
         </div>
       </label>
 
@@ -33,6 +34,23 @@ function CartWidget ({ ...props }) {
         </div>
       </div> */}
     </div>
+  )
+}
+
+function CartIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' className='w-5 h-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z' /></svg>
+  )
+}
+
+function CartBadge ({ children }) {
+  return (
+    <span
+      className={`font-extrabold border-0 text-brand bg-background badge badge-sm indicator-item
+                  sm:text-background sm:bg-foreground`}
+    >
+      {children}
+    </span>
   )
 }
 
