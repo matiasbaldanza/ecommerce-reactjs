edae84345e7d15a21be6e8e8997f2dcb8bc0c9ca
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-16 22:18:20 -0300

    style: Footer: En mobile, mostrar un degradado abajo (fixed) para que se vean bien los elementos posicionados abajo

--- src/modules/layout/Footer/Footer.jsx
+++ src/modules/layout/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer () {
   return (
     <footer className='px-4'>
-      <p className='pt-4 pb-16 mt-10 text-sm text-center border-t sm:py-4 sm:text-md text-muted-foreground'>
+      <p className='z-50 pt-4 mt-10 text-sm text-center border-t pb-28 sm:py-4 sm:text-md text-muted-foreground'>
         Hecho con 🧉, React.js, Tailwind y Firebase en 2023 por{' '}
         <a
           className='underline'
@@ -13,6 +13,11 @@ function Footer () {
         </a>
         .
       </p>
+
+      {/* En mobile, mostrar un degradado abajo (fixed) para que se vean bien los elementos posicionados abajo */}
+      <div
+        className='fixed bottom-0 left-0 z-40 w-full h-24 bg-gradient-to-t from-background via-background to-transparent sm:hidden'
+      />
     </footer>
   )
 }
