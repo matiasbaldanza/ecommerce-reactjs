a0fdf901b7b99d4a60a8131025c79248794a72e3
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-19 12:48:59 -0300

    style: cambiar render condicional de cart para el carro vacío y adaptar a que ocupe flex-1

--- src/modules/cart/Cart/Cart.jsx
+++ src/modules/cart/Cart/Cart.jsx
@@ -2,6 +2,9 @@ import { useContext } from 'react'
 import { CartContext } from '@/context/CartContext'
 import { Link, useNavigate } from 'react-router-dom'
 
+// Notificaciones
+import toast from 'react-hot-toast'
+
 // Componentes
 import CartItem from '../CartItem/CartItem'
 import Price from '../../item/Price'
@@ -9,48 +12,55 @@ import IconCross from '@/modules/ui/Icons/IconCross'
 
 function Cart () {
   const { cart, clearCart, cartQuantity, cartTotalAmount } = useContext(CartContext)
-
   const navigate = useNavigate()
-
-  if (cartQuantity() === 0) {
-    return (
-      <div className='flex flex-col items-center justify-center h-screen'>
-        <h1 className='text-4xl font-bold'>El carrito está vacío</h1>
-        <button onClick={() => navigate(-1)}>Volver</button>
-      </div>
-    )
-  }
 
   return (
     <div className='flex flex-col items-center w-full gap-4 px-4'>
-      <table className='table w-full max-w-4xl table-zebra'>
-        {/* head */}
-        <thead className='text-center'>
-          <tr>
-            <th />
-            <th>Producto</th>
-            <th>Cant.</th>
-            <th>Precio</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map(item => <CartItem key={item.id} {...item} />)}
-        </tbody>
-        <tfoot>
-          <tr>
-            <th />
-            <th />
-            <th className='text-right'><h3>Total: </h3></th>
-            <th className='text-right'><Price className='text-xl text-info ' price={cartTotalAmount()} currency='ARS' /></th>
-          </tr>
-        </tfoot>
-      </table>
+      {
+        cartQuantity() === 0 &&
+          <div className='flex flex-col items-center justify-center h-96'>
+            <h1 className='text-4xl font-bold'>El carrito está vacío</h1>
+            <button onClick={() => navigate(-1)}>Volver</button>
+          </div>
+      }
+      {
+        cartQuantity() > 0 &&
+          <>
+            <table className='table w-full max-w-4xl table-zebra'>
+              {/* head */}
+              <thead className='text-center'>
+                <tr>
+                  <th />
+                  <th>Producto</th>
+                  <th>Cant.</th>
+                  <th>Precio</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map(item => <CartItem key={item.id} {...item} />)}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th />
+                  <th />
+                  <th className='text-right'><h3>Total: </h3></th>
+                  <th className='text-right'><Price className='text-xl text-info ' price={cartTotalAmount()} currency='ARS' /></th>
+                </tr>
+              </tfoot>
+            </table>
+            <button
+              className='flex gap-4 btn btn-error btn-outline' onClick={() => {
+                clearCart()
+                toast.success(<p>Carrito eliminado.</p>)
+              }}
+            >
+              <IconCross />
+              Limpiar carrito
+            </button>
+            <Link className='btn btn-primary' to='/checkout'>Finalizar compra</Link>
+          </>
+    }
 
-      <button className='flex gap-4 btn btn-error btn-outline' onClick={() => clearCart()}>
-        <IconCross />
-        Limpiar carrito
-      </button>
-      <Link className='btn btn-primary' to='/checkout'>Finalizar compra</Link>
     </div>
   )
 }
