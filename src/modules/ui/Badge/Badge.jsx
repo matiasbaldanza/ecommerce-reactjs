a30047b7290fa39c3407f61c7d8cb2dec0a2ef78
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-19 19:22:54 -0300

    refactor: extender Badge para que acepte props y props.className

--- src/modules/ui/Badge/Badge.jsx
+++ src/modules/ui/Badge/Badge.jsx
@@ -1,8 +1,12 @@
-function Badge ({ children }) {
+import clsx from 'clsx'
+
+function Badge ({ children, ...props }) {
+  const baseStyles = `font-extrabold border-0 text-brand bg-background badge badge-sm indicator-item
+                      sm:text-background sm:bg-foreground`
   return (
     <span
-      className={`font-extrabold border-0 text-brand bg-background badge badge-sm indicator-item
-                  sm:text-background sm:bg-foreground`}
+      className={clsx(baseStyles, props.className)}
+      {...props}
     >
       {children}
     </span>
