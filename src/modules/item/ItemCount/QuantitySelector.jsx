1fc4e5e1a9b12278bad30a2ba9e40b973b4bab6f
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-21 17:43:52 -0300

    style: agregar un modo wide a QuantitySelector (no testeado)

--- src/modules/item/ItemCount/QuantitySelector.jsx
+++ src/modules/item/ItemCount/QuantitySelector.jsx
@@ -2,11 +2,11 @@ import clsx from 'clsx'
 import { removeNumberInputControls } from '@/styles/styleUtils'
 
 /* Para usar con el custom hook useQuantity */
-function QuantitySelector ({ quantity, min = 1, max, step = 1, handleIncrement, handleDecrement, compact = false }) {
+function QuantitySelector ({ quantity, min = 1, max, step = 1, handleIncrement, handleDecrement, compact = false, wide = true }) {
   const buttonCompactStyle = clsx(compact ? 'btn-sm' : 'btn-md')
   const inputCompactStyle = clsx(compact ? 'input-sm' : 'input-md')
   const buttonStyle = 'absolute top-0 w-6 btn btn-primary'
-  const inputStyle = 'text-center input input-bordered arrow-hide px-12 w-full'
+  const inputStyle = clsx('text-center input input-bordered arrow-hide px-12', wide && 'w-full')
 
   return (
     <>
