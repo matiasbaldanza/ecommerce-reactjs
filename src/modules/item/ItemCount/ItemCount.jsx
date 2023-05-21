e173e744558e697a0db41c1bbb4f63efc45ea8e7
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-20 22:39:33 -0300

    refactor: ItemCount ahora usa IconButton para el botón 'Agregar al carrito'

--- src/modules/item/ItemCount/ItemCount.jsx
+++ src/modules/item/ItemCount/ItemCount.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
-import clsx from 'clsx'
 import { removeNumberInputControls } from '@/styles/styleUtils'
+import clsx from 'clsx'
+
+// Componentes
+import CartIcon from '@/modules/ui/Icons/CartIcon'
+import IconButton from '@/modules/ui/IconButton'
 
 function ItemCount ({ initial, stock, onAdd }) {
   const [quantity, setQuantity] = useState(initial)
@@ -22,7 +26,7 @@ function ItemCount ({ initial, stock, onAdd }) {
           <button
             className='absolute top-0 left-0 w-6 rounded-r-none btn btn-primary'
             onClick={decrement}
-          >-
+          > -
           </button>
           <input
             type='number'
@@ -36,21 +40,18 @@ function ItemCount ({ initial, stock, onAdd }) {
           <button
             className='absolute top-0 right-0 w-6 rounded-l-none btn btn-primary'
             onClick={increment}
-          >+
+          > +
           </button>
         </>
       )}
       {stock > 0 && (
-        <button
-          className='w-full mt-2 btn btn-md btn-primary'
+        <IconButton
+          className='w-full mt-2 btn-primary btn-sm sm:btn-md'
+          icon={<CartIcon />}
           onClick={() => onAdd(quantity, stock)}
-          disabled={!stock}
         >
-          Agregar
-          <span className='ml-2'>
-            <svg xmlns='http://www.w3.org/2000/svg' className='w-5 h-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z' /></svg>
-          </span>
-        </button>
+          Agregar&nbsp;&nbsp;
+        </IconButton>
       )}
     </div>
   )
